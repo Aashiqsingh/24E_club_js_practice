497e2bebcd977c4209264e1d3f93c87112b00f46
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-04-01 18:54:25 +0530

    basic

---
+++ basic/basic1.js
@@ -0,0 +1,42 @@
+// console.log("Hello world!");
+// console.info("Information function...");
+// console.warn("warning function");
+// console.error("Error fucntion..")
+
+// datatype = number , string , boolean , undefined , object
+
+a = 10;
+console.log("value of a = ",a);
+console.log("type of a = ",typeof(a));
+
+b = 12.2;
+
+console.log("value of b = ",b);
+console.log("type of b = ",typeof(b));
+
+c = "ram";
+console.log("value of c = ",c);
+console.log("type of c = ",typeof(c));
+
+isAlive = true;
+console.log("value of isAlive = ",isAlive);
+console.log("type of isAlive = ",typeof(isAlive));
+
+var d;
+console.log("value of d = ",d);
+console.log("type of d = ",typeof(d));
+
+var e = null;
+console.log("value of e = ",e);
+console.log("type of e = ",typeof(e));
+
+
+
+
+
+
+
+
+
+
+
---
+++ operator/operator.js
@@ -0,0 +1,13 @@
+// console.log(3 ** 4);
+
+var a = '10';
+var b = '10';
+
+console.log(a == b);  // true
+console.log(a != b);
+console.log(a === b); // true
+console.log(a !== b);
+
+
+
+
